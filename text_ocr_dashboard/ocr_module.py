27c29d33668ffd0b8832e4c06a5fd753561e40e7
LegendCHI <141933250+LegendCHI@users.noreply.github.com>
2025-07-22 21:15:39 -0400

    Add files via upload

--- text_ocr_dashboard/ocr_module.py
+++ text_ocr_dashboard/ocr_module.py
@@ -1,15 +1,14 @@
 from PIL import Image, ImageFilter, ImageOps
 import pytesseract
-import numpy as np
 
-pytesseract.pytesseract.tesseract_cmd = r'/usr/bin/tesseract'  # En Streamlit Cloud ya está instalado
-
-def preprocess_image_pil(img: Image.Image):
-    gray = ImageOps.grayscale(img)
-    blur = gray.filter(ImageFilter.GaussianBlur(1))  # Nivel de difuminado
+# En Streamlit Cloud pytesseract ya está instalado y configurado
+def preprocess_image_pil(pil_img):
+    gray = ImageOps.grayscale(pil_img)
+    blur = gray.filter(ImageFilter.GaussianBlur(1))
     return blur
 
 def extract_text_from_image(uploaded_img):
     pil_img = Image.open(uploaded_img)
     processed = preprocess_image_pil(pil_img)
-    return pytesseract.image_to_string(processed)
+    text = pytesseract.image_to_string(processed)
+    return text
